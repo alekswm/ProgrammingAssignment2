0d4366e5f139992c841f8402a767c50565eaa56b
Aleksander Westphal Muniz <alekswm@gmail.com>
2015-10-16 09:06:24 -0400

    Programming Assignment 2 - inverse matrix
    makeCacheMatrix <- function(x = matrix()) {
    inv <- NULL
    set <- function(z) {
    x <<- z
    inv <<- NULL
    }
    get <- function() x
    setinverse <- function(inverse) inv <<- inverse
    getinverse <- function() inv
    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
    }
    cacheSolve <- function(x, ...) {
    inv <- x$getinverse()
    if(!is.null(inv)) {
    
    return(inv)
    }
    data <- x$get()
    inv <- solve(data)
    x$setinverse(inv)
    inv
    }
    
    ## Sample run:
    x = rbind(c(2, 1), c(1, 2))
    m = makeCacheMatrix(x)
    m$get()
    
    ##No cache in the first run
    cacheSolve(m)
    
    ## Retrieving from the cache in the second run
    cacheSolve(m)
    
    > makeCacheMatrix <- function(x = matrix()) {
    + inv <- NULL
    + set <- function(z) {
    + x <<- z
    + inv <<- NULL
    + }
    + get <- function() x
    + setinverse <- function(inverse) inv <<- inverse
    + getinverse <- function() inv
    + list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
    + }
    > cacheSolve <- function(x, ...) {
    + inv <- x$getinverse()
    + if(!is.null(inv)) {
    + return(inv)
    + }
    + data <- x$get()
    + inv <- solve(data)
    + x$setinverse(inv)
    + inv
    + }
    > ## Sample run:
    > x = rbind(c(2, 1), c(1, 2))
    > m = makeCacheMatrix(x)
    > m$get()
    [,1] [,2]
    [1,]    2    1
    [2,]    1    2
    > ##No cache in the first run
    > cacheSolve(m)
    [,1]       [,2]
    [1,]  0.6666667 -0.3333333
    [2,] -0.3333333  0.6666667
    > ## Retrieving from the cache in the second run
    > cacheSolve(m)
    [,1]       [,2]
    [1,]  0.6666667 -0.3333333
    [2,] -0.3333333  0.6666667
    >

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(z) {
+    x <<- z
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+## Sample run:
+x = rbind(c(2, 1), c(1, 2))
+m = makeCacheMatrix(x)
+m$get()
+
+##No cache in the first run
+cacheSolve(m)
+
+## Retrieving from the cache in the second run
+cacheSolve(m)
+
+> makeCacheMatrix <- function(x = matrix()) {
++ inv <- NULL
++ set <- function(z) {
++ x <<- z
++ inv <<- NULL
++ }
++ get <- function() x
++ setinverse <- function(inverse) inv <<- inverse
++ getinverse <- function() inv
++ list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
++ }
+> cacheSolve <- function(x, ...) {
++ inv <- x$getinverse()
++ if(!is.null(inv)) {
++ return(inv)
++ }
++ data <- x$get()
++ inv <- solve(data)
++ x$setinverse(inv)
++ inv
++ }
+> ## Sample run:
+> x = rbind(c(2, 1), c(1, 2))
+> m = makeCacheMatrix(x)
+> m$get()
+     [,1] [,2]
+[1,]    2    1
+[2,]    1    2
+> ##No cache in the first run
+> cacheSolve(m)
+           [,1]       [,2]
+[1,]  0.6666667 -0.3333333
+[2,] -0.3333333  0.6666667
+> ## Retrieving from the cache in the second run
+> cacheSolve(m)
+           [,1]       [,2]
+[1,]  0.6666667 -0.3333333
+[2,] -0.3333333  0.6666667
+> 
